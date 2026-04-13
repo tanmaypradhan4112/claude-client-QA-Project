1f6acf65f682b78363b0c986c6ddd488db0b85e3
tanmaypradhan4112 <tanmaypradhan4112@gmail.com>
2026-04-14 00:21:33 +0530

    LoginPage:  added method

---
+++ pages/LoginPage.js
@@ -0,0 +1,26 @@
+// This file contains Page Object Model for Login Page of Sauce Demo Application
+
+import { testData } from '../utils/testdata';
+
+export class LoginPage {
+
+    // Initializing the locators for username, password and login button
+    constructor(loginpage) {
+        this.loginpage = loginpage;
+        this.usernameInput = loginpage.locator('[data-test="username"]');
+        this.passwordInput = loginpage.locator('[data-test="password"]');
+        this.loginButton = loginpage.locator('[data-test="login-button"]');
+    }
+
+    // Method to navigate to login page
+    async navigateToLoginPage() {
+        await this.loginpage.goto(testData.url.loginUrl);
+    }
+
+    // Method to perform login action
+    async login(username, password) {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+}
