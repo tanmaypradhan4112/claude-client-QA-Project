3cd7a0881bc3aa7660fadcf551b19f71f3fd4f35
tanmaypradhan4112 <tanmaypradhan4112@gmail.com>
2026-04-17 21:57:23 +0530

    inventory

--- pages/InventoryPage.js
+++ pages/InventoryPage.js
@@ -1,7 +1,8 @@
 import { testData } from "../utils/testdata";
+import { expect } from '@playwright/test';
 
-export class InventoryPage{
-    constructor(inventorypage){
+export class InventoryPage {
+    constructor(inventorypage) {
         this.inventorypage = inventorypage;
         this.burgerMenuButton = inventorypage.getByRole('button', { name: 'Open Menu' });
         this.logoutlink = inventorypage.locator('[data-test="logout-sidebar-link"]');
@@ -16,7 +17,7 @@ export class InventoryPage{
 
         // Shop Cart icon / badge
         this.cartbadge = inventorypage.locator('[data-test="shopping-cart-badge"]');
-        
+
         // Add-to-cart & Remove Button
         this.backpack_atcButton = inventorypage.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
         this.bikelight_atcButton = inventorypage.locator('[data-test="add-to-cart-sauce-labs-bike-light"]');
@@ -25,13 +26,22 @@ export class InventoryPage{
         this.onesie_atcButton = inventorypage.locator('[data-test="add-to-cart-sauce-labs-onesie"]');
         this.redtshirt_atcButton = inventorypage.locator('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]');
 
-        remove-sauce-labs-backpack
         this.backpack_removeButton = inventorypage.locator('[data-test="remove-sauce-labs-backpack"]');
         this.bikelight_removeButton = inventorypage.locator('[data-test="remove-sauce-labs-bike-light"]');
         this.bolttshirt_removeButton = inventorypage.locator('[data-test="remove-sauce-labs-bolt-t-shirt"]');
         this.jacket_removeButton = inventorypage.locator('[data-test="remove-sauce-labs-fleece-jacket"]');
         this.onesie_removeButton = inventorypage.locator('[data-test="remove-sauce-labs-onesie"]');
         this.redtshirt_removeButton = inventorypage.locator('[data-test="remove-test.allthethings()-t-shirt-(red)"]');
+
+        // Product
+        this.product_card = inventorypage.locator('[data-test="inventory-item"]');
+        this.product_title = inventorypage.locator('[data-test="inventory-item-name"]');
+        this.product_desc = inventorypage.locator('[data-test="inventory-item-desc"]');
+        this.product_price = inventorypage.locator('[data-test="inventory-item-price"]');
+        this.product_img = inventorypage.locator('img.inventory_item_img');
+
+        // Filter component
+        this.filter_comp = inventorypage.locator('[data-test="product-sort-container"]')
     }
 
     // Method to navigate to inventory page    
@@ -43,5 +53,35 @@ export class InventoryPage{
     async logout() {
         await this.burgerMenuButton.click();
         await this.logoutlink.click();
+    }
+
+    // Method to access all 6 "Add to cart" button via index
+    async addToCartButton(i) {
+        await this.inventorypage.locator('[data-test^="remove"]').nth(i).click();
+    }
+
+    // Method to access all the 6 "Remove" button via index
+    async removeButton(i) {
+        await this.inventorypage.locator('[data-test^="add-to-cart"]').nth(i).click();
+    }
+
+    // Method to select dropdown options
+    async filterDropdownOption(value) {
+        await this.filter_comp.selectOption({ index: value });
+    }
+
+    // Validate Product content
+    async expectProductToMatch(index, expected) {
+        await expect.soft(this.product_title.nth(index)).toHaveText(expected.title);
+        await expect.soft(this.product_desc.nth(index)).toHaveText(expected.description);
+        await expect.soft(this.product_price.nth(index)).toHaveText(expected.price);
+
+        const img = await this.product_img.nth(index).getAttribute('src');
+        expect.soft(img).toBe(expected.img);
+    }
+
+    // Method to take screenshot for Inventory Page Test cases
+    async Takescreenshot(path, name) {
+        await this.inventorypage.screenshot({ path: `${path}${name}.png`, fullPage: true });
     }
 }
